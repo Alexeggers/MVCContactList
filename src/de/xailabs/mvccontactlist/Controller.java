bdd37c33ee8bf18fbe8192c8dedf7dd786ffe24a
Alex.Eggers <alex.f.eggers@gmail.com>
2015-07-15 15:17:06 +0200

    Wrote toTableVector method

--- src/de/xailabs/mvccontactlist/Controller.java
+++ src/de/xailabs/mvccontactlist/Controller.java
@@ -4,8 +4,20 @@ import java.util.Vector;
 
 public class Controller {
 	
+	SQL connection = new SQL();
+	
 	public Vector<Vector<String>> convertToTableVector(Vector<Contact> contactVector) {
+		Vector<Vector<String>> tableVector = new Vector<Vector<String>>();
+		Vector<String> intermediaryVector;
 		
-		return null;
+		for(Contact contact : contactVector) {
+			intermediaryVector = new Vector<String>();
+			intermediaryVector.add(contact.getId());
+			intermediaryVector.add(contact.getName());
+			intermediaryVector.add(contact.getPhonenumber());
+			intermediaryVector.add(contact.getNotes());
+			tableVector.add(intermediaryVector);
+		}
+		return tableVector;
 	}
 }
