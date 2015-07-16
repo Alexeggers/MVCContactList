a3ef25200679a3e2e08f9c00d051cc86dc2b7ea1
Alex.Eggers <alex.f.eggers@gmail.com>
2015-07-16 10:12:34 +0200

    Streamlined order of object creation

--- src/de/xailabs/mvccontactlist/Main.java
+++ src/de/xailabs/mvccontactlist/Main.java
@@ -3,10 +3,9 @@ package de.xailabs.mvccontactlist;
 public class Main {
 
 	public static void main(String[] args) {
-		Controller controller = new Controller();
 		View view = new SwingGUI();
 		SQL connection = new SQL();
-		controller.setView(view);
-		controller.setSQL(connection);
+		Controller controller = new Controller(view, connection);
+		view.buildGUI();
 	}
 }
