e1de323e0c678c87ce7a0130ba54696411a5df53
Alex.Eggers <alex.f.eggers@gmail.com>
2015-07-16 16:26:47 +0200

    Added serial to buttonframe

--- src/de/xailabs/mvccontactlist/ButtonFrame.java
+++ src/de/xailabs/mvccontactlist/ButtonFrame.java
@@ -4,6 +4,10 @@ import javax.swing.JFrame;
 
 public class ButtonFrame extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6074235324460299227L;
 	Contact contact;
 	
 	public ButtonFrame() {
