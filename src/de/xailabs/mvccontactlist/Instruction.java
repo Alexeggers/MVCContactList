fd1a8728486c8ddd72a33701bf439da49fa8f21d
Alex.Eggers <alex.f.eggers@gmail.com>
2015-07-17 15:17:57 +0200

    Built instruction class

---
+++ src/de/xailabs/mvccontactlist/Instruction.java
@@ -0,0 +1,25 @@
+package de.xailabs.mvccontactlist;
+
+import java.awt.Color;
+
+import javax.swing.JTextField;
+
+public class Instruction extends JTextField {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8560986966516913115L;
+	public Instruction (String text, int columns) {
+		super(text, columns);
+		this.setEditable(false);
+		this.setBorder(null);
+		this.setBackground(Color.WHITE);
+		
+	}
+	public Instruction (String text) {
+		super(text);
+		this.setEditable(false);
+		this.setBorder(null);
+		this.setBackground(Color.WHITE);
+	}
+}
